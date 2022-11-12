c52eb5e26b1e365fc5e88994af8b711a6588c8c4
Prabhu2000 <95089893+Prabhu2000@users.noreply.github.com>
2022-11-12 21:07:22 +0530

    Add files via upload

---
+++ MilesConverter/index1.js
@@ -0,0 +1,15 @@
+(function(){
+    'user strict';
+    document.getElementById('convert').addEventListener('submit' , function(event){
+        event.preventDefault();
+        const distance =parseFloat( document.getElementById('distance').value);
+        const answer = document.getElementById('answer');
+        if(distance){
+            const conversion = (distance * 1.609344).toFixed(3);
+            answer.innerHTML= `<h2>${distance} miles converts into ${conversion}kilometers </h2>`;
+        }
+        else{
+            answer.innerHTML = '<h2>PLEASE PROVIDE A NUMBER</h2>';
+        }
+    });
+})();
